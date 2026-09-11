223e3fc02a80caf7394a325cec282b05d4aa7987
bonychriss <bonychriss@users.noreply.github.com>
2026-09-11 10:11:17 +0300

    Auto-sync 2026-09-11 10:11:16

--- modules/petty-cash/categories/create.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require_once __DIR__ . '/../includes/petty-cash-lib.php';
-
-header('Location: ' . pettyCashModuleUrl('categories/index.php'));
-exit;
--- modules/petty-cash/chk.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h1>CONNECTION SUCCESSFUL</h1>";
-echo "<p>This file is in: " . __FILE__ . "</p>";
-echo "<p>Server Time: " . date('Y-m-d H:i:s') . "</p>";
-?>
